0056085f9fce4be748d7c3768f538434fde70e68
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2025-07-29 23:14:37 -0300

    code cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 //import styles from "./page.module.css";
 import styles from "./page.module.css";
 //import asd from "./resources.arsc";
@@ -9,10 +9,18 @@ import styles from "./page.module.css";
 //const fs = require('fs');
 import { promises as fs } from 'fs';
 
-export default function Home() {
+function Home() {
 
   //const [hexContent, setHexContent] = useState('');
-  const [hexContent, setHexContent] = useState([]);
+  //const [hexContent, setHexContent] = useState([]);
+
+  // const [hexContent, setHexContent] = useState([
+  //   ['02', '00', '0c', '00', '5c', '93', 'fa', '00', '04', '00', '00', '00', '01', '00', '1c', '00'],
+  //   ['84', '64', 'a8', '00', 'f6', '52', '02', '00', '1b', '05', '00', '00', '00', '01', '00', '00'],
+  //   ['60', '60', '09', '00', 'cc', '12', 'a8', '00', '00', '00', '00', '00', '48', '00', '00', '00'],
+  //   ['90', '00', '00', '00', '98', '00', '00', '00', 'b6', '00', '00', '00', 'cb', '00', '00', '00'],
+  //   ['f0', '00', '00', '00', '00', '01', '00', '00', '17', '01', '00', '00', '48', '01', '00', '00'],
+  // ]);
   //const fileInputRef = useRef(null);
 
   const handleFileChange = (event) => {
@@ -22,38 +30,37 @@ export default function Home() {
       reader.onload = (e) => {
         const arrayBuffer = e.target.result;
         //const hexString = arrayBufferToHex(arrayBuffer);
+        const uint8Array = new Uint8Array(arrayBuffer);
+        //const hexString = [];
+        const hexString = Array.from({ length: Math.ceil(uint8Array.length / 16) }, () => 
+        []
+        );
+        
 
-         const uint8Array = new Uint8Array(arrayBuffer);
-        const hexString = [];
+
+
         //for (let i = 0; i < uint8Array.length; i + 8) {
           //for (let i = 0; i < uint8Array.length; i++) {
-          for (let i = 0; i < uint8Array.length; i++) {
-            //const byte = uint8Array[i];
-            //hexString += byte.toString(16).padStart(2, '0');
-            //let innerArr = []
-            //innerArr.push(uint8Array[i].toString(16).padStart(2, '0'))
-            //hexString[Math.floor(i / 16)] = uint8Array[i].toString(16).padStart(2, '0')
-            //hexString[Math.floor(i / 4)] = uint8Array[i].toString(16).padStart(2, '0')
-            //hexString[0] = uint8Array[i].toString(16).padStart(2, '0')
-            //hexString[Math.floor(i / 16)] = uint8Array[i].toString(16).padStart(2, '0')
-            //hexString[Math.floor(i / 16)][i] = uint8Array[i].toString(16).padStart(2, '0')
-            //hexString[Math.floor(i / 16)].push(uint8Array[i].toString(16).padStart(2, '0'))
-            const target = i / 16;
-            const target2 = i % 16;
-            if (target2 === 0) hexString[Math.floor(target)] = [uint8Array[i].toString(16).padStart(2, '0')]
-            else hexString[Math.floor(target)].push(uint8Array[i].toString(16).padStart(2, '0'))
 
+          // for (let i = 0; i < uint8Array.length; i++) {
+          //   const target = i / 16;
+          //   const target2 = i % 16;
+          //   if (target2 === 0) hexString[Math.floor(target)] = [uint8Array[i].toString(16).padStart(2, '0')]
+          //   else hexString[Math.floor(target)].push(uint8Array[i].toString(16).padStart(2, '0'))
 
-            //console.log("33", uint8Array[i])
-            //console.log("33", uint8Array[i].toString(16).padStart(2, '0'))
-            //console.log("33", Math.floor(i / 16))
-            ///hexString.push(uint8Array[j].toString(16).padStart(2, '0'))
-          }
-        //}
-        //return hexString;
+          // }
 
+          // for (let i = 0; i < uint8Array.length; i++) {
+          //   const target = i / 16;
+          //   // const target2 = i % 16;
+          //   // if (target2 === 0) hexString[Math.floor(target)] = [uint8Array[i].toString(16).padStart(2, '0')]
+          //   // else hexString[Math.floor(target)].push(uint8Array[i].toString(16).padStart(2, '0'))
+          //   //hexString[Math.floor(target)][] = uint8Array[i].toString(16).padStart(2, '0')
+          //   //hexString[Math.floor(target)].push(uint8Array[i].toString(16).padStart(2, '0'))
+          //   hexString[Math.floor(target)].push(uint8Array[i].toString(16).padStart(2, '0'))
+          // }
 
-        setHexContent(hexString);
+        //setHexContent(hexString);
       };
       reader.readAsArrayBuffer(file);
     }
@@ -73,17 +80,71 @@ export default function Home() {
   //   return hexString;
   // };
 
+  //const qq = hexContent.map((e: [], i) =>  <div key={i} style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>{e.join(' ')}</div>)
+  //const qq = hexContent.map((e: [], i) =>  <div key={i} style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>{e.join(' ')}</div>)
+
+  // const canvas = document.getElementById('myCanvas');
+  // let ctx;
+  // if (canvas !== null) ctx = canvas.getContext('2d');
+  // //const ctx = canvas.getContext('2d');
+
+  // const cellWidth = 50;
+  // const cellHeight = 50;
+
+  // for (let row = 0; row < hexContent.length; row++) {
+  //   for (let col = 0; col < hexContent[row].length; col++) {
+  //       const value = hexContent[row][col];
+  //       const x = col * cellWidth;
+  //       const y = row * cellHeight;
+
+  //       // Customize drawing based on the cell's value
+  //       if (value === 1) {
+  //           ctx.fillStyle = 'blue';
+  //       } else {
+  //           ctx.fillStyle = 'lightgray';
+  //       }
+
+  //       ctx.fillRect(x, y, cellWidth, cellHeight);
+  //       ctx.strokeStyle = 'black'; // Optional: Add a border
+  //       ctx.strokeRect(x, y, cellWidth, cellHeight);
+  //       //ctx.fillText('My text', x, y);
+  //   }
+  // }
+
   return (
     <div>
       <input type="file" /* ref={fileInputRef} */ onChange={handleFileChange} />
       {hexContent && (
         <div>
           <h3>Hexadecimal Content:</h3>
-          <div style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
-            {/* {hexContent.map(e => e)} */}
-            {/* {hexContent.join(' ')} */}
-            {/* {hexContent.toString()} */}
-            {/* {hexContent.join(' ')} */}
+          <div style={{ whiteSpace: 'pre-wrap', fontFamily: 'monospace', fontSize: 'large' }}>
+
+            {/* {
+              hexContent.map((e: string[], i) => {
+                return <div style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }} key={i}>{e.join(' ')}</div>
+              })
+            } */}
+
+            {/* <canvas id="myCanvas" width="400" height="400"></canvas> */}
+            {/* <div style={{width: '100%', border: '1px solid black', whiteSpace: 'pre-wrap', wordBreak: 'break-all' }} >
+              {hexContent.map((row: [], rowIndex) => ( // Skip the header row
+                row.toString()
+              ))}
+            </div> */}
+
+            {
+              /* hexContent[Symbol.iterator]() */
+              //hexContent.join(' ')
+              //hexContent.join("\n")
+              /* hexContent.map((iA: [], idx) => iA.join(`${idx} `)).join(`\n`) */
+              //hexContent.map((iA: [], idx) => { return iA.join(` `) } ).join(`\n`)
+              //hexContent.map((iA: [], idx) => { return `${idx} ${iA}` } ).join(`\n`)
+              hexContent.map((item: [], index) => {
+                return `${index + 1}. ${item.join(' ')}`; // Adds 1 to index for 1-based numbering
+              }).join("\n")
+            }
+
+
           </div>
         </div>
       )}
@@ -91,3 +152,6 @@ export default function Home() {
   );
 
 }
+
+export default Home;
+//export default React.memo(Home);
