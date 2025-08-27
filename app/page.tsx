49560352c3a2aa0ef1c882b06d54aa03d29e9458
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2025-08-27 17:00:29 -0300

    code cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,6 @@ function Home() {
   const arrHex = useRef<string[][]>([['']])
   const arrInd = useRef<string[][]>([['']])
 
-  //const [ indices, setIndices ] = useState<string[][]>([]); // ←←← DON'T USE IT!!
-
-  //const [ indicesTest, setIndicesTest ] = useState<string[]>([]);
-
-  //const currentIndex = useRef(0)
-
-  //console.log("currentIndex.current", currentIndex.current)
-
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -45,49 +37,27 @@ function Home() {
           hexDivRef.current.textContent = `${arrHex.current[0].join(' ')} ${arrHex.current[1].join(' ')} ` // NEW! work at start
           hexDivRef.current.style.paddingBottom = `${(4105-1) * 4000}px`
           indDivRef.current.style.paddingBottom = `${(4105-1) * 4000}px`
-
-          
-
         }
-        
-
       };
       reader.readAsArrayBuffer(file);
     }
   };
 
-  //console.log("last indice", indices[indices.length -1])
-
   let padStart// = indices && indices[indices.length - 1].toString().length
-
-  //if (indices[indices.length - 1]) padStart = indices[indices.length - 1].toString().length
-  //if (indices[indices.length - 1]) padStart = indices[indices.length - 1].toString().length + 1
 
   console.log("padStart", padStart)
 
-  //console.log("indicesTest", indicesTest)
-  //console.log("indicesTest length", indicesTest.length)
-  //console.log("indicesTest last", indicesTest[0])
-
   useEffect(() => {
-    window.addEventListener('scroll', function(){
-
+    function updateScrollPosition(this: Window) {
+      console.log("EVENT EVENT", this)
       console.log("scroll curr", this.scrollY-32)
       console.log("thumb height", this.innerHeight)
       console.log("scroll curr + thumb height", (this.scrollY-32)+this.innerHeight)
       console.log("scroll curr + thumb height / 4000", ((this.scrollY-32)+this.innerHeight)/4000)
       const qq = Math.floor(((this.scrollY-32) + (this.innerHeight)) / 4000)
-      //console.log("scroll curr + thumb", qq) // TRACKING
 
-      //setCurrentIndex(qq)
-      //currentIndex.current = qq ?? 0
       console.log("qq", qq) // TRACKING
       if (hexDivRef.current &&  indDivRef.current) { // ON SCROLL
-        //divRef.current.style.paddingTop = `${qq === 0 ? 0 : ((qq-1) * 4000)}px`
-        
-        ////divRef.current.style.paddingBottom = `${(4106-(qq+2)) * 4000}px` // OLD! work at start
-        ////divRef.current.style.paddingBottom = `${(4105-(qq+1)) * 4000}px` // NEW! work at start
-
         indDivRef.current.style.paddingTop = `${qq > 1 ? ((qq-1) * 4000) : 0 }px` // OLD! work at start
         indDivRef.current.style.paddingBottom = `${qq < 4104 ? (4105-(qq+1)) * 4000 : 0}px` // NEW! work at start
 
@@ -96,35 +66,18 @@ function Home() {
 
         indDivRef.current.textContent = `${qq > 0 ? arrInd.current[qq-1].join(' ') : ''} ${qq > 4105 ? '' : arrInd.current[qq].join(' ')} ${qq > 4104 ? '' : arrInd.current[qq+1].join(' ')} ` // NEW! work at start
         hexDivRef.current.textContent = `${qq > 0 ? arrHex.current[qq-1].join(' ') : ''} ${qq > 4105 ? '' : arrHex.current[qq].join(' ')} ${qq > 4104 ? '' : arrHex.current[qq+1].join(' ')} ` // NEW! work at start
-        
       }
-    }, { passive: true });
+    }
 
-    // document.addEventListener('scroll', function(){
-    //   //console.log("document", this)
-    //   console.dir(this)
-    //   //console.log("position iH", window.innerHeight)
-    //   //console.log("position", window)
-    // }, { passive: true });
-
-    return () => {
-      window.removeEventListener('scroll', updateScrollPosition);
-    };
+    window.addEventListener('scroll', updateScrollPosition, { passive: true });
+    return () => window.removeEventListener('scroll', updateScrollPosition);;
   }, []);
 
   const hexDivRef = useRef(null)
   const indDivRef = useRef(null)
 
   console.log("A VER ESTE", hexDivRef.current)
-  //console.log(divRef.current)
   console.dir(hexDivRef.current)
-
-  //console.log("indicesTest lllength", indicesTest.length)
-  //console.log("indices lllength 2 ", indicesTest.length * 4000)
-
-  //if (indicesTest[0] !== undefined) console.log("indices[0] :::", indicesTest[0])
-  
-  if (arrHex.current[1]) console.log("WWW")
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column'}}>
@@ -136,7 +89,6 @@ function Home() {
           padding: `0 1ch`,
           boxSizing: 'content-box',
         }} />
-        
         <div ref={hexDivRef} style={{
           background: 'lavender', lineHeight: '16px',
           fontFamily: 'monospace', fontSize: '16px',
